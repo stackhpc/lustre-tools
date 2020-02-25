2df4fac7fb3425e7b5f7c6ecd2c04a5dbe7bd55a
Steve Brasier <steveb@stackhpc.com>
2020-02-25 10:38:02 +0000

    parse nested lists/dicts

--- nodemap.py
+++ nodemap.py
@@ -9,28 +9,47 @@
 from __future__ import print_function
 __version__ = "0.0"
 
-import subprocess, pprint, sys
+import subprocess, pprint, sys, re, ast
 
 def cmd(args):
     proc = subprocess.Popen(args, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE) # need shell else lctl not found
-    #proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     return stdout, stderr
 
 def lctl_get_param(item, output):
     s, e = cmd("sudo lctl get_param '{item}'".format(item=item)) # need quoting around `item` to avoid shell expansion of ".*" !
     lines = s.strip().split('\n')
+    accumulate = []
     for line in lines:
-        if line: # skip empty liens
-            parts = line.split('.')
-            if parts[0] == 'nodemap':
+        #print(line)
+        if line: # skip empty lines
+            #print('line:', line)
+            if '=' in line:
+                
+                # handle accumulated value lines from *previous* object:
+                if accumulate:
+                    prev_value = r[param] + ''.join(accumulate) # sometimes previous key=value ended"=[" so always prefix that
+                    quoted_prev_value = re.sub(r'\s?([^\s:]+):\s?([^\s,]+)', r"'\1':'\2'", prev_value) # add quoting around dict values and keys
+                    # turn it into python:
+                    try:
+                        py_prev_value = ast.literal_eval(quoted_prev_value)
+                    except:
+                        print('ERROR: failed when parsing', quoted_prev_value)
+                        raise
+                    accumulate = []
+                    r[param] = py_prev_value
+
+                # handle normal lines:
+                parts = line.split('.')
                 param, value = parts[-1].split('=')
                 r = output
                 for p in parts[:-1]:
                     r = r.setdefault(p, {})
                 r[param] = value
+
+
             else:
-                print('WARNING: skipping line', line, file=sys.stderr) # FIXME: 
+                accumulate.append(line)
     return output
     
 def main():
