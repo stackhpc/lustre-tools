stackhpc/lustre-tools

commit 76cabed59a730abc8df7a1497b1e662ec2d7621c

fix for nodemap when values contain '.'

nodemap.py
@@ -70,8 +70,8 @@ def lctl_get_param(item, output):
                     accumulate = []
 
                 # handle normal lines:
-                parts = line.split('.')
-                param, value = parts[-1].split('=')
+                param, _ , value = line.partition('=')
+                parts = param.split('.')
                 r = output
                 for p in parts[:-1]:
                     r = r.setdefault(p, {})

commit f3d191d761f651c32ad6efb826b140e6ea52d1e2

bugfix error in nested keys introduced in 76cabed59a730abc8df7a1497b1e662ec2d7621c

nodemap.py
@@ -78,10 +78,12 @@ def lctl_get_param(item, output):
                     accumulate = []
 
                 # handle normal lines:
-                param, _ , value = line.partition('=')
-                parts = param.split('.')
+                dotted_param, _ , value = line.partition('=')
+                parts = dotted_param.split('.')
+                parents = parts[:-1]
+                param = parts[-1]
                 r = output
-                for p in parts[:-1]:
+                for p in parents:
                     r = r.setdefault(p, {})
                 r[param] = value
 