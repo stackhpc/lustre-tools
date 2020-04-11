4a48f7d8df0c255102cce3d1d51cff407bc5ca58
Steve Brasier <steveb@stackhpc.com>
2020-04-11 15:39:38 +0000

    import now returns (non-zero) returncode if lctl command fails

--- nodemap.py
+++ nodemap.py
@@ -70,6 +70,8 @@ def cmd(cmdline):
 def call(cmdline):
     proc = subprocess.Popen(cmdline, shell=True) # need shell else lctl not found
     proc.wait()
+    if proc.returncode != 0:
+        exit(proc.returncode)
 
 def lctl_get_param(item, output):
     """ Get a lustre parameter.
